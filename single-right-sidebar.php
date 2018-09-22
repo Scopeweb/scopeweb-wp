2a4eecb46584a1a19287762aac2141d4e473eef7
Stephan Moerman <stephan@scopeweb.nyc>
2018-09-23 01:59:49 +0200

    Finished adding final template files

---
+++ single-right-sidebar.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Single Post Template: Right Sidebar
+*/
+?>
+<?php get_header(); ?>
+<?php if ( ! function_exists( 'scope_ajax_enqueue_scripts' )) { scope_setPostViews( get_the_ID() );} ?>
+<?php get_sidebar('before-post'); ?>
+    <div id="container" class="clearfix">
+        <div id="content" class="float-left">
+            <?php while ( have_posts() ) : the_post(); ?>
+                <?php get_template_part( 'content', 'single' ); ?>
+                <?php
+                if ( comments_open() || get_comments_number() ) {
+                    comments_template();
+                } ?>
+            <?php endwhile; ?>
+        </div>
+        <div itemscope itemtype="http://schema.org/WPSideBar" id="sidebar" class="float-right">
+            <?php get_sidebar('blog'); ?>
+        </div>
+    </div><!-- #container -->
+<?php get_footer(); ?>
---
+++ single.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+<?php if ( ! function_exists( 'scope_ajax_enqueue_scripts' )) { scope_setPostViews( get_the_ID() );} ?>
+<?php get_sidebar('before-post'); ?>
+	<div id="container">
+		<div id="content">
+			<?php while ( have_posts() ) : the_post(); ?>				
+				<?php get_template_part( 'content', 'single' ); ?>
+					<?php
+					if ( comments_open() || get_comments_number() ) {
+						comments_template();
+					} ?>	
+				<?php endwhile; ?>	
+		</div><!-- #content -->				
+	</div><!-- #container -->
+<?php get_footer(); ?>
