c868daf7548fb0264aa69a34eeb0222ca61e8f97
Stephan Moerman <stephan@scopeweb.nyc>
2018-09-23 01:32:51 +0200

    Finished adding sidebar files

---
+++ sidebar-post-content-top.php
@@ -0,0 +1,14 @@
+<?php
+/*	
+*	---------------------------------------------------------------------
+*	SCOPE Post Content Top Sidebar
+*	--------------------------------------------------------------------- 
+*/
+?>
+<?php if ( is_active_sidebar( 'post-content-top-sidebar' ) ) : ?>
+	<aside id="post-content-top-sidebar" class="clearfix">
+		<div class="post-widget-area">
+			<?php dynamic_sidebar( 'post-content-top-sidebar' ); ?>
+		</div>
+	</aside>
+<?php endif; ?>	
---
+++ sidebar-shop.php
@@ -0,0 +1,12 @@
+<?php
+/*	
+*	---------------------------------------------------------------------
+*	SCOPE Default page sidebar
+*	--------------------------------------------------------------------- 
+*/
+?>
+<div class="page-sidebar">
+	<div class="widget-area">
+		<?php dynamic_sidebar( 'shop-widget-area' ); ?>
+	</div>
+</div><!-- .page-sidebar -->
